d767530315f1b9612b245bc293df322c4ac162de
Dmitrij Michljuk <>
2024-02-08 23:20:45 +0100

    Add poll/topic/votes

--- bot/commands/common.py
+++ bot/commands/common.py
@@ -5,7 +5,7 @@ from bot.logging import log
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    await update.message.reply_text("Hi, I am alive! Hello world!")
+    await update.message.reply_text("Hi, I am alive! Hello world! 2")
     return ConversationHandler.END
 
 
