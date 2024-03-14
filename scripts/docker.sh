ca11bc351cf753680558f74685b5efc5b4ada464
Nikolay Khodov <i@valt.me>
2024-03-14 01:18:19 +0100

    Dockerize app

---
+++ scripts/docker.sh
@@ -0,0 +1,11 @@
+#!/use/bin/env bash
+
+if [ ! -e /venv/bin/activate ]; then
+  python -m venv /venv
+fi
+
+source /venv/bin/activate
+
+pip install -r /requirements.txt
+python /manage.py migrate
+daphne -b 0.0.0.0 -p 8080 vercel_app.asgi:application
