70e3a9fe19e5ee5546ec2101180bbf051bd189a2
Nikolay Khodov <i@valt.me>
2024-03-15 23:04:31 +0100

    Properly retract votes

--- bot/helpers/retract_first_vote.py
+++ bot/helpers/retract_first_vote.py
@@ -1,10 +1,15 @@
-from bot.models import TelegramUser, Training, Attendee
+from bot.models import TelegramUser, Training, Attendee, POLL_VOTE_SOURCE
 from asgiref.sync import sync_to_async
 
 
 async def retract_first_vote(training: Training, telegram_user: TelegramUser):
     first_poll_vote = await sync_to_async(
-        lambda: Attendee.objects.filter(training=training, telegram_user=telegram_user)
+        lambda: Attendee.objects.filter(
+            training=training,
+            telegram_user=telegram_user,
+            go=True,
+            source=POLL_VOTE_SOURCE,
+        )
         .order_by("id")
         .first()
     )()
